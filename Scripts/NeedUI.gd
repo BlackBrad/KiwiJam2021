4089361cbdcc4bd8e4508d1f1b39e3ab4a6905dc
David Goosen <david@davidgoosen.kiwi>
2021-07-25 14:16:34 +1200

    Implement need change indicators

--- Scripts/NeedUI.gd
+++ Scripts/NeedUI.gd
@@ -8,12 +8,29 @@ export(NodePath) var need_source_path
 
 onready var label = $Label
 onready var progress_bar = $ProgressBar
+onready var increase_indicator = $ProgressBar/IncreaseArrow
+onready var decrease_indicator = $ProgressBar/DecreaseArrow
+
+var _old_amount = 0.0
 
 func _ready():
 	label.text = label_name
+	increase_indicator.hide()
+	decrease_indicator.hide()
 
-func _process(delta):
+func _physics_process(delta):
 	var need_source = get_node(need_source_path)
 	var source_amount = need_source.get_needs_value(need_key)
 	progress_bar.value = int(source_amount)
 
+	if source_amount - _old_amount > 0.01:
+		increase_indicator.show()
+		decrease_indicator.hide()
+	elif source_amount - _old_amount < -0.01:
+		decrease_indicator.show()
+		increase_indicator.hide()
+	else:
+		increase_indicator.hide()
+		decrease_indicator.hide()
+
+	_old_amount = source_amount
