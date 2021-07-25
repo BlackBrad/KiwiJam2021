e59b027e34e284213c2b1dbf8ef43e27adbd97a5
RivetingStuff <kylemt94@live.com>
2021-07-25 14:49:59 +1200

    Added substancecs to spawner dict
    Added repellant to tv

--- Scripts/SubstanceSpawner.gd
+++ Scripts/SubstanceSpawner.gd
@@ -10,17 +10,74 @@ const spawn_table = [
 	{ 
 		'substance': Globals.Substances.OATS,
 		'amount': 100,
-		'rate': 20
+		'rate': 17,
+		'label': Color(1, 0, 0, 1)
 	},
 	{ 
 		'substance': Globals.Substances.SOY_SAUCE,
 		'amount': 100,
-		'rate': 20
+		'rate': 20,
+		'label': Color(1, 0, 0, 1)
 	},
 	{ 
 		'substance': Globals.Substances.HOT_MILK,
 		'amount': 100,
-		'rate': 40
+		'rate': 30,
+		'label': Color(1, 0, 0, 1)
+	},
+		{ 
+		'substance': Globals.Substances.AGGREGATE,
+		'amount': 100,
+		'rate': 30,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.CIGGIES,
+		'amount': 100,
+		'rate': 25,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.EFFLUENT,
+		'amount': 100,
+		'rate': 20,
+		'label': Color(1, 0, 0, 1)
+	},
+		{ 
+		'substance': Globals.Substances.FONDANT,
+		'amount': 100,
+		'rate': 15,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.GOBLINS,
+		'amount': 100,
+		'rate': 11,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.MILDEW,
+		'amount': 100,
+		'rate': 30,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.PINKBATTS,
+		'amount': 100,
+		'rate': 15,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.REPELLANT,
+		'amount': 100,
+		'rate': 35,
+		'label': Color(1, 0, 0, 1)
+	},
+	{ 
+		'substance': Globals.Substances.WASPS,
+		'amount': 100,
+		'rate': 20,
+		'label': Color(1, 0, 0, 1)
 	},
 ]
 
@@ -36,7 +93,7 @@ func on_timeout():
 	substance.amount = spawn_table[index]['amount']
 	substance.rate = spawn_table[index]['rate']
 	substance.y_bounce = self.global_position.y
-	substance.label_color = Color(1, 0, 0, 1)
+	substance.label_color = spawn_table[index]["label"]
 	substance.connect('on_empty', self, 'on_empty')
 	add_child(substance)
 	substance.global_position.y -= rand_range(150, 300)
