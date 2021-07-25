c66fd45e88e82bb51206c473dc66674975f28866
RivetingStuff <kylemt94@live.com>
2021-07-25 14:24:49 +1200

    Added many needs, many substances
    The TV now abstracts into tv shows. All three support all substances

--- Scenes/TV.gd
+++ Scenes/TV.gd
@@ -8,19 +8,69 @@ var source = weakref(null)
 
 var current_effect = {}
 
-var effect_status = {
-	Globals.Substances.NONE: {},
-	Globals.Substances.HOT_MILK: {
-		Globals.Needs.A: 3.0,
-		Globals.Needs.B: -3.0
+enum TV_Shows {
+	DRY,
+	ACTION,
+	EROTIC,
+	MASH,
+	NEWS,
+	DOOLITTLE,
+	NATURE
+}
+
+var tv_effects = {
+	TV_Shows.DRY: {
+		Globals.Needs.Aridity: -1.0,
+		Globals.Needs.Stimulation: -2.0,
+		Globals.Needs.Rapaciousness: -0.5,
 	},
-	Globals.Substances.OATS: {
-		Globals.Needs.COMFORT: -3.0
+	TV_Shows.ACTION:{
+		Globals.Needs.Stimulation: 3.0,
+		Globals.Needs.Repose: -1.0,
 	},
-	Globals.Substances.SOY_SAUCE: {
-		Globals.Needs.COMFORT: 2.0,
-		Globals.Needs.TEMP: -1.0
+	TV_Shows.EROTIC: {
+		Globals.Needs.Pyrexia: 3.0,
+		Globals.Needs.Unwasp: -1.0,
+		Globals.Needs.Repose: -1.0,
+		Globals.Needs.Stimulation: 3.0
+	},
+	TV_Shows.MASH: {
+		Globals.Needs.Aridity: 2.0,
+		Globals.Needs.Rapaciousness: 3.0,
+		Globals.Needs.Stimulation: 1.2
+	},
+	TV_Shows.NEWS: {
+		Globals.Needs.Stimulation: -3.0,
+		Globals.Needs.Aridity: -1.5,
+		Globals.Needs.Pyrexia: -2.0
+	},
+	TV_Shows.DOOLITTLE: {
+		Globals.Needs.Arcaena: 3.5,
+		Globals.Needs.Unwasp: -1.5,
+		Globals.Needs.Repose: 3.2,
+		Globals.Needs.Stimulation: 2.7
+	},
+	TV_Shows.NATURE: {
+		Globals.Needs.Unwasp: -1.5,
+		Globals.Needs.Aridity: 2.0,
+		Globals.Needs.Arcaena: -0.5,
+		Globals.Needs.Repose: 1.5,
+		Globals.Needs.Stimulation: -1.5
 	}
+}
+
+var tv_map = {
+	Globals.Substances.OATS: TV_Shows.DRY,
+	Globals.Substances.SOY_SAUCE: TV_Shows.MASH,
+	Globals.Substances.HOT_MILK: TV_Shows.EROTIC,
+	Globals.Substances.WASPS: TV_Shows.DOOLITTLE,
+	Globals.Substances.GOBLINS: TV_Shows.DOOLITTLE,
+	Globals.Substances.PINKBATTS: TV_Shows.DRY,
+	Globals.Substances.AGGREGATE: TV_Shows.NEWS,
+	Globals.Substances.MILDEW: TV_Shows.NATURE,
+	Globals.Substances.EFFLUENT: TV_Shows.NEWS,
+	Globals.Substances.FONDANT: TV_Shows.MASH,
+	Globals.Substances.CIGGIES: TV_Shows.EROTIC,
 }
 
 
@@ -32,10 +82,13 @@ func on_connect_substance(substance_source):
 	source = substance_source
 
 func _physics_process(delta):
+	current_effect.clear()
 	if source.get_ref():
 		var substance = source.get_ref().drain_substance(delta)
-		for key in effect_status[substance]:
-			current_effect[key] = effect_status[substance][key]
+		if substance != Globals.Substances.NONE:
+			var tv_mapping = tv_map[substance]
+			for key in tv_effects[tv_mapping]:
+				current_effect[key] = tv_effects[tv_mapping][key]
 
 func get_effect():
 	return current_effect
--- Scripts/NeedUI.gd
+++ Scripts/NeedUI.gd
@@ -3,7 +3,7 @@ extends Control
 const Globals = preload('res://Scripts/Globals.gd')
 
 export(String) var label_name = "<NEED NAME>"
-export(Globals.Needs) var need_key = Globals.Needs.A
+export(Globals.Needs) var need_key = Globals.Needs.Rapaciousness
 export(NodePath) var need_source_path
 
 onready var label = $Label
@@ -11,9 +11,8 @@ onready var progress_bar = $ProgressBar
 
 func _ready():
 	label.text = label_name
-
+	
 func _process(delta):
 	var need_source = get_node(need_source_path)
 	var source_amount = need_source.get_needs_value(need_key)
 	progress_bar.value = int(source_amount)
-
